4cbe3723715ea3d55d02148cee264337342576ec
VintageAppMaker <adsloader99@gmail.com>
2021-04-11 17:53:08 +0900

    .

--- app/src/main/java/com/psw/adsloader/githubsearcher/MainActivity.kt
+++ app/src/main/java/com/psw/adsloader/githubsearcher/MainActivity.kt
@@ -8,14 +8,9 @@ import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.psw.adsloader.githubsearcher.adapter.GithubAdapter
-import com.psw.adsloader.githubsearcher.api.api
 import com.psw.adsloader.githubsearcher.databinding.ActivityMainBinding
-import com.psw.adsloader.githubsearcher.model.Repo
 import com.psw.adsloader.githubsearcher.util.toast
 import com.psw.adsloader.githubsearcher.view.MainActivityData
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -24,7 +19,6 @@ import android.text.InputType
 import android.view.inputmethod.EditorInfo
 import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
-import com.psw.adsloader.githubsearcher.api.IORoutine
 import com.psw.adsloader.githubsearcher.model.GithubData
 
 
@@ -65,7 +59,7 @@ class MainActivity : AppCompatActivity() {
 
                         if(nNextPage == IS_END_PAGE) return
                         toast("다음페이지를 읽습니다.")
-                        loadRepoInfoWithPage()
+                        viewmodel.loadRepoInfoWithPage()
                     }
                 }
             }
@@ -128,111 +122,6 @@ class MainActivity : AppCompatActivity() {
             { dialog, which -> dialog.cancel() })
 
         builder.show()
-
-    }
-
-    private fun toNextPageWithEnd(bIsEnd : Boolean = false ){
-         if(nNextPage != IS_END_PAGE ) nNextPage++
-         if( bIsEnd )
-             nNextPage = IS_END_PAGE
-    }
-
-    private fun loadUserInfo() {
-        nNextPage = FIRST_PAGE
-        viewmodel.bLoading.postValue(true)
-
-        // 코투틴과 Retrofit 사용방법을 위한 예제
-        // UI처리는 반드시 LiveData로 보낸다.
-        // 그렇게 하지않으면 Context간의 차이로 App이 종료됨
-        IORoutine({
-            val u = api.function.getUser(viewmodel.account.value.toString())
-            if(u == null) return@IORoutine
-
-            // 데이터처리
-            var items = mutableListOf<GithubData>().apply{
-                add(u as GithubData)
-            }
-
-            // UI에 전송
-            viewmodel.lst.postValue(items)
-        }, {
-            viewmodel.bLoading.postValue(false)
-            viewmodel.message.postValue("$it")
-        })
-
-    }
-
-    private fun loadRepoInfo() {
-        nNextPage = FIRST_PAGE
-        viewmodel.bLoading.postValue(true)
-
-        IORoutine({
-            val l = api.function.listRepos(viewmodel.account.value.toString())
-            if(l == null){
-                viewmodel.bLoading.postValue(false)
-                toNextPageWithEnd(true)
-                return@IORoutine
-            }
-
-            if(l.size < 1){
-                viewmodel.bLoading.postValue(false)
-                toNextPageWithEnd(true)
-                return@IORoutine
-            }
-
-            toNextPageWithEnd()
-
-            l?.forEachIndexed { index, repo ->   repo.name = "${index}.${repo.name}" }
-
-            viewmodel.lst.postValue(l)
-            viewmodel.title.postValue("${adapter.mItems.size -1} repositories")
-            viewmodel.bLoading.postValue(false)
-
-        }, {
-            viewmodel.bLoading.postValue(false)
-            viewmodel.message.postValue("$it")
-        })
-
-    }
-
-    private fun loadRepoInfoWithPage() {
-        viewmodel.bLoading.postValue(true)
-
-        // 코루틴을 사용하지 않는방법
-        api.function.listReposWithPage(viewmodel.account.value.toString(), nNextPage).enqueue( object: Callback<List<Repo>>{
-
-            override fun onFailure(call: Call<List<Repo>>, t: Throwable) {
-                viewmodel.bLoading.postValue(false)
-            }
-
-            override fun onResponse(call: Call<List<Repo>>, response: Response<List<Repo>>) {
-                val repos = response.body()
-
-                if(repos == null){
-                    viewmodel.bLoading.postValue(false)
-                    toNextPageWithEnd(true)
-                    return
-                }
-
-                if(repos.size < 1){
-                    viewmodel.bLoading.postValue(false)
-                    toNextPageWithEnd(true)
-                    return
-                }
-
-                toNextPageWithEnd()
-
-                repos?.let{
-                    it.forEachIndexed { index, repo ->   repo.name = "${ adapter.mItems.size + index}.${repo.name}" }
-                    adapter.addItems(it)
-                }
-
-                viewmodel.title.postValue("${adapter.mItems.size - 1} repositories")
-                viewmodel.bLoading.postValue(false)
-
-            }
-
-        })
 
     }
 
--- app/src/main/java/com/psw/adsloader/githubsearcher/model/MainViewModel.kt
+++ app/src/main/java/com/psw/adsloader/githubsearcher/model/MainViewModel.kt
@@ -2,6 +2,12 @@ package com.psw.adsloader.githubsearcher.model
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.psw.adsloader.githubsearcher.MainActivity
+import com.psw.adsloader.githubsearcher.api.IORoutine
+import com.psw.adsloader.githubsearcher.api.api
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainViewModel :ViewModel(){
     var bLoading : MutableLiveData<Boolean> =  MutableLiveData()
@@ -14,5 +20,112 @@ class MainViewModel :ViewModel(){
     init{
         bLoading.value = false
     }
+
+    fun loadUserInfo() {
+        MainActivity.nNextPage = MainActivity.FIRST_PAGE
+        bLoading.postValue(true)
+
+        // 코투틴과 Retrofit 사용방법을 위한 예제
+        // UI처리는 반드시 LiveData로 보낸다.
+        // 그렇게 하지않으면 Context간의 차이로 App이 종료됨
+        IORoutine({
+            val u = api.function.getUser(account.value.toString())
+            if(u == null) return@IORoutine
+
+            // 데이터처리
+            var items = mutableListOf<GithubData>().apply{
+                add(u as GithubData)
+            }
+
+            // UI에 전송
+            lst.postValue(items)
+        }, {
+            bLoading.postValue(false)
+            message.postValue("$it")
+        })
+
+    }
+
+    fun loadRepoInfo() {
+        MainActivity.nNextPage = MainActivity.FIRST_PAGE
+        bLoading.postValue(true)
+
+        IORoutine({
+            val l = api.function.listRepos(account.value.toString())
+            if(l == null){
+                bLoading.postValue(false)
+                toNextPageWithEnd(true)
+                return@IORoutine
+            }
+
+            if(l.size < 1){
+                bLoading.postValue(false)
+                toNextPageWithEnd(true)
+                return@IORoutine
+            }
+
+            toNextPageWithEnd()
+
+            l?.forEachIndexed { index, repo ->   repo.name = "${index}.${repo.name}" }
+
+            lst.postValue(l)
+            title.postValue("${lst.value!!.size -1} repositories")
+            bLoading.postValue(false)
+
+        }, {
+            bLoading.postValue(false)
+            message.postValue("$it")
+        })
+
+    }
+
+    fun loadRepoInfoWithPage() {
+        bLoading.postValue(true)
+
+        // 코루틴을 사용하지 않는방법
+        api.function.listReposWithPage(account.value.toString(), MainActivity.nNextPage).enqueue( object:
+            Callback<List<Repo>> {
+
+            override fun onFailure(call: Call<List<Repo>>, t: Throwable) {
+                bLoading.postValue(false)
+            }
+
+            override fun onResponse(call: Call<List<Repo>>, response: Response<List<Repo>>) {
+                val repos = response.body()
+
+                if(repos == null){
+                    bLoading.postValue(false)
+                    toNextPageWithEnd(true)
+                    return
+                }
+
+                if(repos.size < 1){
+                    bLoading.postValue(false)
+                    toNextPageWithEnd(true)
+                    return
+                }
+
+                toNextPageWithEnd()
+
+                lst.postValue(repos)
+
+                repos?.let{
+                    it.forEachIndexed { index, repo ->   repo.name = "${ lst.value!!.size + index}.${repo.name}" }
+                }
+
+                title.postValue("${lst.value!!.size - 1} repositories")
+                bLoading.postValue(false)
+
+            }
+
+        })
+    }
+
+    private fun toNextPageWithEnd(bIsEnd : Boolean = false ){
+        if(MainActivity.nNextPage != MainActivity.IS_END_PAGE) MainActivity.nNextPage++
+        if( bIsEnd )
+            MainActivity.nNextPage = MainActivity.IS_END_PAGE
+    }
+
 
 }
